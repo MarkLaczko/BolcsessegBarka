891455e54be89286afdd1d71a9611e93962929e2
Laczkó Márk <diak_479.laczko.mark@2per14.b.kimeno.com>
2024-03-26 11:38:38 +0100

    Created the HomePage.vue file

--- frontend/src/pages/HomePage.vue
+++ frontend/src/pages/HomePage.vue
@@ -1,5 +1,15 @@
 <template>
+    <BaseLayout>
+    
+    </BaseLayout>
 </template>
 
 <script>
+import BaseLayout from "@layouts/BaseLayout.vue"
+
+export default {
+    components: {
+        BaseLayout
+    }
+}
 </script>
