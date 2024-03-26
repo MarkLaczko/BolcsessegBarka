35d4a831d81b20d8645c448a1f8f6ccee0f4eba2
Kurityák Dániel István <diak_478.kurityak.daniel.istvan@2per14.b.kimeno.com>
2024-03-26 13:49:46 +0000

    Fixed typo in AuthController authenticate action

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -22,7 +22,7 @@ class AuthController extends Controller
         }
 
         return response()->json([
-            "message" => [
+            "data" => [
                 "message" => "Unsuccessful authentication"
             ]
         ], 401);
