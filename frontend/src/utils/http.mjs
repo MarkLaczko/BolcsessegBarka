c57d9b19dbdee1e9d64d2e627c052571f65ec1e7
Kurityák Dániel István <diak_478.kurityak.daniel.istvan@2per14.b.kimeno.com>
2024-03-26 18:35:57 +0000

    Updated axios config to use localhost:8881

--- frontend/src/utils/http.mjs
+++ frontend/src/utils/http.mjs
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const http = axios.create({
-    baseURL: "/api",
+    baseURL: "http://localhost:8881/api",
     headers:{
         "Accept": "application/json",
         "Content-Type": "application/json" 
