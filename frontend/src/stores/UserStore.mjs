1a55d27e5fd5cb018d12aa9b20df04c0a67fedba
Laczkó Márk <diak_479.laczko.mark@2per14.b.kimeno.com>
2024-03-27 19:47:35 +0100

    getUser function: save current user data, logout function: serves for deleting user data

--- frontend/src/stores/UserStore.mjs
+++ frontend/src/stores/UserStore.mjs
@@ -8,12 +8,28 @@ export const userStore = defineStore('userStore', {
             name: '',
             email: '',
             global_role: '',
+            currentUserData: []
         }
     },
     actions:{
+        async getUser(){
+            const response = await http.get("/user", {
+                headers: {
+                    Authorization: `Bearer ${this.token}`
+                }
+            });
+            this.currentUserData = response.data;
+        },
         async login(credentials){
             const response = await http.post('/users/login', credentials)
             this.token = response.data.data.token;
+        },
+        logout(){
+            this.token = '';
+            this.name = '';
+            this.email = '';
+            this.global_role = '';
+            this.currentUserData = [];
         }
     },
     getters:{
