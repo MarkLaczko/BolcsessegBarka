6f03f4726c210a39af5adb002c43d0e218a5ac49
Laczkó Márk <diak_479.laczko.mark@2per14.b.kimeno.com>
2024-04-01 14:37:28 +0200

    Starting the writing of UserResource, supplementing UserController

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -3,12 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreUserRequest;
+use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return UserResource::collection(User::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
     public function store(StoreUserRequest $request){
         $data = $request->validated();
 
@@ -23,5 +35,38 @@ class UserController extends Controller
                 "message" => "Successful registration"
             ]
         ], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $user = User::findOrFail($id);
+
+        return new UserResource($user);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(StoreUserRequest $request, string $id)
+    {
+        $data = $request->validated();
+        $user = User::findOrFail($id);
+        $user->update($data);
+
+        return new UserResource($user);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+
+        return response()->noContent();
     }
 }
