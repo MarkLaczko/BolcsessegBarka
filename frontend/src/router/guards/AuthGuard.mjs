ebe5f33c53aee8884a5ffd4bd5950dea3604e0c1
Laczkó Márk <diak_479.laczko.mark@2per14.b.kimeno.com>
2024-04-02 11:27:33 +0200

    Expanding the AuthGuard: A logged-in user can no longer access the /register and /login routes

--- frontend/src/router/guards/AuthGuard.mjs
+++ frontend/src/router/guards/AuthGuard.mjs
@@ -1,14 +1,15 @@
 import { userStore } from "@stores/UserStore.mjs";
-export function AuthGuard(to, from, next){
-    if(userStore().isAuthenticated){
-        next()
+export function AuthGuard(to, from, next) {
+  if (userStore().isAuthenticated) {
+    if (to.path === "/login" || to.path === "/register") {
+      next("/home");
     }
-    else{
-        if(to.meta.requiresAuth){
-            next({name: 'login'})
-        }
-        else{
-            next()
-        }
+    next();
+  } else {
+    if (to.meta.requiresAuth) {
+      next({ name: "login" });
+    } else {
+      next();
     }
+  }
 }
