c214777842d6a2cab32f7af2b1f6baaa3eaa6c16
Kurityák Dániel István <diak_478.kurityak.daniel.istvan@2per14.b.kimeno.com>
2024-04-02 10:49:02 +0000

    Changed route of /home to / in Vue project

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -25,7 +25,7 @@ export const router = createRouter({
       }
     },
     {
-      path: '/home',
+      path: '/',
       name: 'home',
       component: () => import('@pages/HomePage.vue'),
       meta: {
