114acf47877120eaa8d5c8c5c85195b3a75f35be
Laczkó Márk <diak_479.laczko.mark@2per14.b.kimeno.com>
2024-04-04 16:06:38 +0200

    Rewriting the FormKit configuration for better usability

--- frontend/formkit.config.js
+++ frontend/formkit.config.js
@@ -1,21 +1,20 @@
-import {defaultConfig} from '@formkit/vue'
-import {generateClasses} from "@formkit/themes";
-
+import { defaultConfig } from "@formkit/vue";
+import { generateClasses } from "@formkit/themes";
 
 export default defaultConfig({
-    config: {
-        classes: generateClasses({
-            global: {
-                input: 'form-control',
-                messages: 'list-style-none',
-                message: 'text-danger p-0'
-            },
-            select: {
-                input: 'form-select'
-            },
-            submit: {
-                input: 'btn'
-            },
-        })
-    }
-})
+  config: {
+    classes: generateClasses({
+      global: {
+        messages: "list-style-none",
+        message: "text-danger p-0",
+        checkbox: "form-check",
+      },
+      select: {
+        input: "form-select",
+      },
+      submit: {
+        input: "btn",
+      },
+    }),
+  },
+});
