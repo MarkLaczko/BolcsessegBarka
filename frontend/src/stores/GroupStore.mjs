460e61ac9dac3fe10018787440f1a40a1437af0d
Kurityák Dániel István <diak_478.kurityak.daniel.istvan@2per14.b.kimeno.com>
2024-04-08 13:24:18 +0000

    Added GroupStore

---
+++ frontend/src/stores/GroupStore.mjs
@@ -0,0 +1,54 @@
+import { defineStore } from "pinia";
+import { http } from "@utils/http";
+
+export const groupStore = defineStore("groupStore", {
+    state() {
+        return {
+            groups: [],
+        };
+    },
+    actions: {
+        async getGroups() {
+            const response = await http.get("/groups", {
+                headers: {
+                    Authorization: `Bearer ${this.token}`,
+                },
+            });
+            this.groups = response.data.data;
+        },
+        async getGroup(id) {
+            const response = await http.get(`/groups/${id}`, {
+                headers: {
+                    Authorization: `Bearer ${this.token}`,
+                },
+            });
+            return response.data.data;
+        },
+        async postGroup(data) {
+            const response = await http.get(`/groups`, data, {
+                headers: {
+                    Authorization: `Bearer ${this.token}`,
+                },
+            });
+            this.groups.push(response.data.data);
+        },
+        async putGroup(id, data) {
+            const response = await http.get(`/groups/${id}`, data, {
+                headers: {
+                    Authorization: `Bearer ${this.token}`,
+                },
+            });
+            const idx = this.groups.findIndex(x => x.id == id);
+            this.groups.splice(idx, 1, response.data.data);
+        },
+        async destroyGroup(id) {
+            const response = await http.get(`/groups/${id}`, {
+                headers: {
+                    Authorization: `Bearer ${this.token}`,
+                },
+            });
+            const idx = this.groups.findIndex(x => x.id == id);
+            this.groups.splice(idx, 1);
+        },
+    }
+});
