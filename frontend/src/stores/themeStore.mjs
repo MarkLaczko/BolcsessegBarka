434357513fa6fec2e2ab8d6078064bec3d111a11
Laczkó Márk <diak_479.laczko.mark@2per14.b.kimeno.com>
2024-04-08 11:27:44 +0200

    Corrected the dark mode button's icon bug

--- frontend/src/stores/themeStore.mjs
+++ frontend/src/stores/themeStore.mjs
@@ -9,19 +9,13 @@ export const themeStore = defineStore("themeStore", {
   actions: {
     toggleTheme() {
       this.isDarkMode = !this.isDarkMode;
-      var icon = document.getElementById("icon");
 
       if (this.isDarkMode) {
-        icon.classList.remove("fa-moon");
-        icon.classList.add("fa-sun");
-
         document.documentElement.setAttribute("data-theme", "dark");
       } else {
-        icon.classList.add("fa-moon");
-        icon.classList.remove("fa-sun");
-
         document.documentElement.removeAttribute("data-theme");
       }
     },
   },
+  persist: true,
 });
