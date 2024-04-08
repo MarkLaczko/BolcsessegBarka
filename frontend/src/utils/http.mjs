0488f0ac7c0e3d23647edfac844b53f57302c21b
Laczkó Márk <diak_479.laczko.mark@2per14.b.kimeno.com>
2024-04-08 06:17:44 +0200

    Added http interceptor for unauthenticated users

--- frontend/src/utils/http.mjs
+++ frontend/src/utils/http.mjs
@@ -1,9 +1,24 @@
-import axios from 'axios'
+import axios from "axios";
+import { userStore } from "@stores/UserStore.mjs";
+import { router } from "@/router/index.js";
 
-export const http = axios.create({
-    baseURL: "http://localhost:8881/api",
-    headers:{
-        "Accept": "application/json",
-        "Content-Type": "application/json" 
+const http = axios.create({
+  baseURL: "http://localhost:8881/api",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+http.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response.status === 401) {
+      const store = userStore();
+      store.logout();
+      router.push("/login");
     }
-})
+  }
+);
+
+export { http };
