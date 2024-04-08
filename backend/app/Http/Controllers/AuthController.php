b0096a1273c2dbea2b8fed8880485205a7b677c6
Laczkó Márk <diak_479.laczko.mark@2per14.b.kimeno.com>
2024-04-08 06:15:06 +0200

    Customized the bearer token expiration time

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
@@ -12,7 +11,7 @@ class AuthController extends Controller
         $data = $request->validated();
 
         if(Auth::attempt($data)){
-            $token = $request->user()->createToken('token');
+            $token = $request->user()->createToken('token', ['*'], now()->addMinutes(20));
 
             return response()->json([
                 "data" => [
