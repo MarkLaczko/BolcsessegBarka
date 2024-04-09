704cafd8990b9cd1abd077db72bcd489bbead3ae
Laczkó Márk <diak_479.laczko.mark@2per14.b.kimeno.com>
2024-04-09 14:30:10 +0200

    Writing the functions of the NoteController

---
+++ backend/app/Http/Controllers/NoteController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreNoteRequest;
+use App\Http\Resources\NoteResource;
+use App\Models\Note;
+use Illuminate\Http\Request;
+
+class NoteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return NoteResource::collection(Note::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreNoteRequest $request)
+    {
+        $data = $request->validated();
+        $note = Note::create($data);
+
+        return new NoteResource($note);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $note = Note::findOrFail($id);
+
+        return new NoteResource($note);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(StoreNoteRequest $request, string $id)
+    {
+        $data = $request->validated();
+        $note = Note::findOrFail($id);
+        $note->update($data);
+
+        return new NoteResource($note);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $note = Note::findOrFail($id);
+        $note->delete();
+
+        return new NoteResource($note);
+    }
+}
