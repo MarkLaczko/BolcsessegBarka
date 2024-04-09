12378e19d5e60f97c32ffae10abb10b67a6d422b
Laczkó Márk <diak_479.laczko.mark@2per14.b.kimeno.com>
2024-04-09 14:27:33 +0200

    Added toast for successful/unsuccessful note saving

--- frontend/src/pages/NotePage.vue
+++ frontend/src/pages/NotePage.vue
@@ -1,5 +1,34 @@
 <template>
   <BaseLayout>
+    <Toast
+      :pt="{
+        root: {
+          class: 'w-25',
+        },
+        detail: {
+          class: 'text-center',
+        },
+        icon: {
+          class: 'mt-1 ms-1',
+        },
+        text: {
+          class: 'w-75 mx-auto',
+        },
+        container: {
+          class: ' rounded w-75',
+        },
+        buttonContainer: {
+          class: 'w-25 d-flex justify-content-center ms-auto',
+        },
+        button: {
+          class: 'btn mb-2',
+        },
+        transition: {
+          name: 'slide-fade',
+        },
+      }"
+    />
+
     <div class="container rounded-3 my-5 pt-1 pb-3">
       <h1 class="text-center my-3">{{ this.$route.meta.title }}</h1>
       <div class="d-flex align-items-center justify-content-center pb-4">
@@ -35,6 +64,8 @@ import BaseLayout from "@layouts/BaseLayout.vue";
 import InputText from "primevue/inputtext";
 import Editor from "primevue/editor";
 import Button from "primevue/button";
+import Toast from "primevue/toast";
+import { http } from "@utils/http.mjs";
 
 export default {
   data() {
@@ -48,6 +79,47 @@ export default {
     InputText,
     Editor,
     Button,
+    Toast,
+  },
+  methods: {
+    async saveNote() {
+      try {
+        const note = {
+          title: this.title,
+          text: this.text,
+        };
+
+        const response = await http.post("/api/notes", note, {
+          //TODO: Backend részről megírni az API-t.
+          headers: {
+            Authorization: `Bearer ${this.token}`,
+          },
+        });
+
+        if (response.status === 200) {
+          let toast = {
+            severity: "success",
+            detail: "Jegyzet elmentése sikeres volt!",
+            life: 3000,
+          };
+          if (!this.isDarkMode) {
+            toast.styleClass = "bg-success text-white";
+          }
+
+          this.$toast.add(toast);
+        }
+      } catch (error) {
+        let toast = {
+          severity: "error",
+          detail: "Jegyzet elmentése sikertelen volt!",
+          life: 3000,
+        };
+        if (!this.isDarkMode) {
+          toast.styleClass = "bg-danger text-white";
+        }
+        this.$toast.add(toast);
+      }
+    },
   },
 };
 </script>
