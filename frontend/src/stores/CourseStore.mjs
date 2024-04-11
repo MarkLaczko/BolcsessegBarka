6c802a2ee1488e3057e4467c89e2114db22b8eb2
Molnár Gábor <diak_482.molnar.gabor@2per14.b.kimeno.com>
2024-04-11 07:49:19 +0200

    Adding UserStore for CourseStore and using the user token

--- frontend/src/stores/CourseStore.mjs
+++ frontend/src/stores/CourseStore.mjs
@@ -1,5 +1,6 @@
 import { defineStore } from "pinia";
 import { http } from "@utils/http";
+import { userStore } from '@stores/UserStore'
 
 export const courseStore = defineStore("courseStore", {
     state() {
@@ -9,42 +10,47 @@ export const courseStore = defineStore("courseStore", {
     },
     actions: {
         async getCourses() {
+            const user = userStore();
             const response = await http.get("/courses", {
                 headers: {
-                    Authorization: `Bearer ${this.token}`,
+                    Authorization: `Bearer ${user.token}`,
                 },
             });
             this.courses = response.data.data;
         },
         async getCourse(id) {
+            const user = userStore();
             const response = await http.get(`/courses/${id}`, {
                 headers: {
-                    Authorization: `Bearer ${this.token}`,
+                    Authorization: `Bearer ${user.token}`,
                 },
             });
             return response.data.data;
         },
         async postCourse(data) {
+            const user = userStore();
             const response = await http.get(`/courses`, data, {
                 headers: {
-                    Authorization: `Bearer ${this.token}`,
+                    Authorization: `Bearer ${user.token}`,
                 },
             });
             this.courses.push(response.data.data);
         },
         async putCourse(id, data) {
+            const user = userStore();
             const response = await http.get(`/courses/${id}`, data, {
                 headers: {
-                    Authorization: `Bearer ${this.token}`,
+                    Authorization: `Bearer ${user.token}`,
                 },
             });
             const ids = this.courses.findIndex(x => x.id == id);
             this.courses.splice(ids, 1, response.data.data);
         },
         async destroyCourse(id) {
+            const user = userStore();
             const response = await http.get(`/courses/${id}`, {
                 headers: {
-                    Authorization: `Bearer ${this.token}`,
+                    Authorization: `Bearer ${user.token}`,
                 },
             });
             const ids = this.courses.findIndex(x => x.id == id);
