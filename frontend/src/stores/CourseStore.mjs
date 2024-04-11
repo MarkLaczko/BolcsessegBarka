bb7544c82bf317c443ba468bdc005977d3731f7c
Laczkó Márk <diak_479.laczko.mark@2per14.b.kimeno.com>
2024-04-11 14:59:09 +0200

    Fixed CourseStore: replaced the wrong request type to the right one

--- frontend/src/stores/CourseStore.mjs
+++ frontend/src/stores/CourseStore.mjs
@@ -1,60 +1,60 @@
 import { defineStore } from "pinia";
 import { http } from "@utils/http";
-import { userStore } from '@stores/UserStore'
+import { userStore } from "@stores/UserStore";
 
 export const courseStore = defineStore("courseStore", {
-    state() {
-        return {
-            courses: [],
-        };
+  state() {
+    return {
+      courses: [],
+    };
+  },
+  actions: {
+    async getCourses() {
+      const user = userStore();
+      const response = await http.get("/courses", {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      });
+      this.courses = response.data.data;
     },
-    actions: {
-        async getCourses() {
-            const user = userStore();
-            const response = await http.get("/courses", {
-                headers: {
-                    Authorization: `Bearer ${user.token}`,
-                },
-            });
-            this.courses = response.data.data;
+    async getCourse(id) {
+      const user = userStore();
+      const response = await http.get(`/courses/${id}`, {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
         },
-        async getCourse(id) {
-            const user = userStore();
-            const response = await http.get(`/courses/${id}`, {
-                headers: {
-                    Authorization: `Bearer ${user.token}`,
-                },
-            });
-            return response.data.data;
+      });
+      return response.data.data;
+    },
+    async postCourse(data) {
+      const user = userStore();
+      const response = await http.post(`/courses`, data, {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
         },
-        async postCourse(data) {
-            const user = userStore();
-            const response = await http.get(`/courses`, data, {
-                headers: {
-                    Authorization: `Bearer ${user.token}`,
-                },
-            });
-            this.courses.push(response.data.data);
+      });
+      this.courses.push(response.data.data);
+    },
+    async putCourse(id, data) {
+      const user = userStore();
+      const response = await http.put(`/courses/${id}`, data, {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
         },
-        async putCourse(id, data) {
-            const user = userStore();
-            const response = await http.get(`/courses/${id}`, data, {
-                headers: {
-                    Authorization: `Bearer ${user.token}`,
-                },
-            });
-            const ids = this.courses.findIndex(x => x.id == id);
-            this.courses.splice(ids, 1, response.data.data);
+      });
+      const ids = this.courses.findIndex((x) => x.id == id);
+      this.courses.splice(ids, 1, response.data.data);
+    },
+    async destroyCourse(id) {
+      const user = userStore();
+      const response = await http.delete(`/courses/${id}`, {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
         },
-        async destroyCourse(id) {
-            const user = userStore();
-            const response = await http.get(`/courses/${id}`, {
-                headers: {
-                    Authorization: `Bearer ${user.token}`,
-                },
-            });
-            const ids = this.courses.findIndex(x => x.id == id);
-            this.courses.splice(ids, 1);
-        },
-    }
+      });
+      const ids = this.courses.findIndex((x) => x.id == id);
+      this.courses.splice(ids, 1);
+    },
+  },
 });
