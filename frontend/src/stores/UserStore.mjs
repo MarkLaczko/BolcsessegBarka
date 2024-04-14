5b5fc6f6cd7ada9e17fa87fd6557452025b0bb9a
Laczkó Márk <diak_479.laczko.mark@2per14.b.kimeno.com>
2024-04-14 12:18:45 +0200

    Writing a getter for displaying a monogram

--- frontend/src/stores/UserStore.mjs
+++ frontend/src/stores/UserStore.mjs
@@ -35,6 +35,18 @@ export const userStore = defineStore("userStore", {
         this.currentUserData != undefined && this.currentUserData.is_admin === 1
       );
     },
+    avatarText() {
+      if (!this.currentUserData || !this.currentUserData.name) {
+        return "";
+      }
+
+      const names = this.currentUserData.name.split(" ").filter(Boolean);
+      if (names.length > 1) {
+        return names[0][0].toUpperCase() + names[1][0].toUpperCase();
+      }
+
+      return names[0][0].toUpperCase();
+    },
   },
   persist: true,
 });
