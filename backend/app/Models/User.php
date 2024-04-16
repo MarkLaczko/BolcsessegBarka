a08d805ff3e58b4b45f7b6f38c836c0f39632c01
Kurityák Dániel István <diak_478.kurityak.daniel.istvan@2per14.b.kimeno.com>
2024-04-16 13:53:14 +0000

    Hid unneccessary attributes in User model

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -22,7 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        "is_admin"
+        'is_admin'
     ];
 
     /**
@@ -33,6 +33,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'email_verified_at',
+        'created_at',
+        'updated_at'
     ];
 
     /**
