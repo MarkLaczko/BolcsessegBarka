33329f647a80b73b1d3910c8004c659e4ea43fa1
Kurityák Dániel István <diak_478.kurityak.daniel.istvan@2per14.b.kimeno.com>
2024-04-18 23:49:09 +0200

    Added Member class for API test

---
+++ tests/BolcsessegBarkaAPITests/ApiLibrary/Member.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace ApiLibrary;
+
+public class Member
+{
+    [JsonProperty("group_id")]
+    public int GroupId { get; set; }
+    
+    [JsonProperty("user_id")]
+    public int UserId { get; set; }
+
+    [JsonProperty("permission")]
+    public string Permission { get; set; }
+}
