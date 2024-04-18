b6831be7b5569b0e410824055efd4942e94b438a
Laczkó Márk <diak_479.laczko.mark@2per14.b.kimeno.com>
2024-04-18 12:16:12 +0200

    Moving polymorphic relationships to the topic model, Implementing the hasMany relationship towards the topic table

--- backend/app/Models/Course.php
+++ backend/app/Models/Course.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Course extends Model
@@ -19,18 +20,8 @@ class Course extends Model
         return $this->belongsToMany(Group::class, "course_group");
     }
 
-    public function notes() : MorphMany
+    public function topics() : HasMany 
     {
-        return $this->morphMany(Note::class, "courseable");
-    }
-
-    public function quizzes() : MorphMany
-    {
-        return $this->morphMany(Quiz::class, "courseable");
-    }
-
-    public function assignments() : MorphMany
-    {
-        return $this->morphMany(Assignment::class, "courseable");
+        return $this->hasMany(Topic::class);
     }
 }
