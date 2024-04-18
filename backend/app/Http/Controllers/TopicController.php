91e044f8b8fc2847b2dd2a753f56ef51a7adf5ae
Laczkó Márk <diak_479.laczko.mark@2per14.b.kimeno.com>
2024-04-18 12:13:10 +0200

    Creating the TopicController with methods: index(), store(), show(), update(), destroy()

---
+++ backend/app/Http/Controllers/TopicController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTopicRequest;
+use App\Http\Requests\UpdateTopicRequest;
+use App\Http\Resources\TopicResource;
+use App\Models\Topic;
+use Illuminate\Http\Request;
+
+class TopicController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return TopicResource::collection(Topic::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTopicRequest $request)
+    {
+        $data = $request->validated();
+        $topic = Topic::create($data);
+
+        return new TopicResource($topic); 
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $topic = Topic::findOrFail($id);
+
+        return new TopicResource($topic);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTopicRequest $request, string $id)
+    {
+        $data = $request->validated();
+        $topic = Topic::findOrFail($id);
+        $topic->update($data);
+
+        return new TopicResource($topic);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $topic = Topic::findOrFail($id);
+        $topic->delete();
+
+        return response()->noContent();
+    }
+}
