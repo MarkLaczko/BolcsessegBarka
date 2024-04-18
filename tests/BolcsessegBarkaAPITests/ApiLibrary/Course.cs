9543acb88e4597eeb253253ffb8c61fc72d77166
Kurityák Dániel István <diak_478.kurityak.daniel.istvan@2per14.b.kimeno.com>
2024-04-18 23:48:00 +0200

    Added Course class for API test

---
+++ tests/BolcsessegBarkaAPITests/ApiLibrary/Course.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace ApiLibrary;
+
+public class Course
+{
+    [JsonProperty("id")]
+    public int Id { get; set; }
+    
+    [JsonProperty("name")]
+    public string Name { get; set; }
+    
+    [JsonProperty("image")]
+    public string Image { get; set; }
+}
