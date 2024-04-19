5af40c86577a473d72a0b2539fddc716084e1545
Laczkó Márk <diak_479.laczko.mark@2per14.b.kimeno.com>
2024-04-19 13:19:39 +0200

    Configuring multilingualism for the Courses page, passing the group name to the BaseCourseCard according to which group belongs to which course

--- frontend/src/pages/CoursesPage.vue
+++ frontend/src/pages/CoursesPage.vue
@@ -1,32 +1,18 @@
 <template>
   <BaseLayout>
-    <div class="d-flex justify-content-center" v-if="loading">
-      <img
-        src="../assets/images/logo.png"
-        alt="logo"
-        width="300px"
-        class="rotating-pulsating"
-      />
-    </div>
+    <BaseSpinner :loading="loading" />
 
     <div class="rounded-3 my-5 pt-2" v-if="!loading">
       <h1 class="text-center mb-4">{{ messages.pages.coursesPage.title }}</h1>
 
       <h2 v-if="courses.length === 0" class="text-center">
-        Nincsenek kurzusaid!
+        {{ messages.pages.coursesPage.youDontHaveCoursesText }}
       </h2>
 
       <div class="row px-5 pb-5 g-4">
-        <div
-          class="col-12 col-lg-4 col-md-6"
-          v-for="course in courses"
-          :key="course.id"
-        >
-          <BaseCourseCard
-            :title="`${course.name}`"
-            :image="`${course.image}`"
-            :courseId="course.id"
-          />
+        <div class="col-12 col-lg-4 col-md-6" v-for="course in courses" :key="course.id">
+          <BaseCourseCard :title="`${course.name}`" :image="`${course.image}`" :courseId="course.id"
+            :groupName="course.groupName" />
         </div>
       </div>
     </div>
@@ -35,11 +21,13 @@
 
 <script>
 import BaseLayout from "@layouts/BaseLayout.vue";
+import BaseSpinner from "@components/BaseSpinner.vue";
 import BaseCourseCard from "@components/BaseCourseCard.vue";
-import { mapState } from "pinia";
+import { mapState, mapActions } from "pinia";
 import { languageStore } from "@stores/LanguageStore.mjs";
 import { http } from "@utils/http.mjs";
 import { userStore } from "@stores/UserStore.mjs";
+import { groupStore } from "@stores/GroupStore.mjs";
 
 export default {
   data() {
@@ -49,6 +37,7 @@ export default {
     };
   },
   methods: {
+    ...mapActions(groupStore, ["getGroups"]),
     async getCourses() {
       const response = await http.get("/groups", {
         headers: {
@@ -56,13 +45,21 @@ export default {
         },
       });
 
+      const coursesWithGroups = [];
       response.data.data.forEach((group) => {
         group.users.forEach((user) => {
           if (user.id === userStore().currentUserData.id) {
-            this.courses.push(...group.courses);
+            group.courses.forEach((course) => {
+              coursesWithGroups.push({
+                ...course,
+                groupName: group.name,
+              });
+            });
           }
         });
       });
+      this.courses = coursesWithGroups;
+      console.log(this.courses);
     },
   },
   computed: {
@@ -75,6 +72,7 @@ export default {
   components: {
     BaseLayout,
     BaseCourseCard,
+    BaseSpinner
   },
 };
 </script>
