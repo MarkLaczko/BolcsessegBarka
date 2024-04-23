5fe5dda5f5e3c7ad80d821caab38ffe0700e11e0
Kurityák Dániel István <diak_478.kurityak.daniel.istvan@2per14.b.kimeno.com>
2024-04-23 13:35:48 +0000

    Added subtasks table migration

---
+++ backend/database/migrations/2024_04_23_103040_create_subtasks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subtasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('task_id')->constrained();
+            $table->integer('order');
+            $table->text('question');
+            $table->text('options')->nullable();
+            $table->text('solution')->nullable();
+            $table->string('type');
+            $table->float('marks');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subtasks');
+    }
+};
