3f7f85febdac3368dee64f8b775377669c29fb04
Kurityák Dániel István <diak_478.kurityak.daniel.istvan@2per14.b.kimeno.com>
2024-04-23 21:46:23 +0000

    Added SubtaskResource

---
+++ backend/app/Http/Resources/SubtaskResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SubtaskResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'order' => $this->order,
+            'question' => $this->question,
+            'options' => $this->options,
+            'type' => $this->type,
+            'marks' => $this->marks,
+            'task' =>new TaskResource($this->whenLoaded('task')),
+        ];
+    }
+}
