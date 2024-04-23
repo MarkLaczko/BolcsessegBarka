48ce99222923a22b2ee8dc0cd5840e6464c446dd
Molnár Gábor <diak_482.molnar.gabor@2per14.b.kimeno.com>
2024-04-23 12:44:24 +0200

    Developed student assignment table with four column (id, assignment_id, student_task_name, student_task)

---
+++ backend/database/migrations/2024_04_22_084817_create_student_assignments_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('student_assignments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("assignment_id")->constrained("assignment","id");
+            $table->string("student_task_name");
+        });
+        DB::statement("ALTER TABLE student_assignments ADD student_task LONGBLOB");
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('student_assignments');
+    }
+};
