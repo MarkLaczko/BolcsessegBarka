12f01896921b3961ad0fe48c3fb69e38fb1b74b1
Kurityák Dániel István <diak_478.kurityak.daniel.istvan@2per14.b.kimeno.com>
2024-04-23 21:44:48 +0000

    Added UpdateQuizRequest

---
+++ backend/app/Http/Requests/UpdateQuizRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateQuizRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'max:100'],
+            'max_attempts' => ['nullable', 'numeric', 'integer', 'min:0'],
+            'opens' => ['nullable', 'numeric', 'integer', 'min:0'],
+            'closes' => ['nullable', 'numeric', 'integer', 'min:0'],
+            'time' => ['nullable', 'numeric', 'integer', 'min:0'],
+            'topic_id' => ['required', 'numeric', 'integer', 'exists:topics,id'],
+        ];
+    }
+}
