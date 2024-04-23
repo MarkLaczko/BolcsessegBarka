d17dfc87166f17e93f6f9ae8516bf807cecbafba
Laczkó Márk <diak_479.laczko.mark@2per14.b.kimeno.com>
2024-04-23 19:15:33 +0200

    Created TopicSeeder

---
+++ backend/database/seeders/TopicSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TopicSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('topics')->insert([
+            [
+                'name' => 'Történelmi korszakok',
+                'order' => 1
+            ],
+            [
+                'name' => 'Függvények',
+                'order' => 2
+            ]
+        ]);
+    }
+}
