7c81c917fa4e6728982037a48a8831f89a7cef33
Molnár Gábor <diak_482.molnar.gabor@2per14.b.kimeno.com>
2024-04-23 12:38:50 +0200

    Writing StudentAssignment resource

---
+++ backend/app/Http/Resources/StudentAssignmentResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class StudentAssignmentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            "assignment_id" => $this->assignment_id,
+            "student_task_name" => $this->student_task_name,
+        ];
+    }
+}
