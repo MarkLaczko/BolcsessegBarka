3ff45e2ee9cb354b7a0354ee6e82e4e58e89144b
Laczkó Márk <diak_479.laczko.mark@2per14.b.kimeno.com>
2024-04-24 10:23:06 +0200

    Establishing a relationship with courses in the user model

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -48,8 +49,13 @@ class User extends Authenticatable
         'password' => 'hashed',
     ];
 
-    public function groups() : BelongsToMany
+    public function groups(): BelongsToMany
     {
         return $this->belongsToMany(Group::class, "member")->as('member')->withPivot('permission');
+    }
+
+    public function created_courses(): HasMany
+    {
+        return $this->hasMany(Course::class, "created_by");
     }
 }
