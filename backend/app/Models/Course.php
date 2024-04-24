c5c66a24645a9628e23174e5abc38c08e8f3ba90
Laczkó Márk <diak_479.laczko.mark@2per14.b.kimeno.com>
2024-04-24 10:22:00 +0200

    "Setting the fillability of the created_by field, establishing a connection with users

--- backend/app/Models/Course.php
+++ backend/app/Models/Course.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 
 class Course extends Model
 {
@@ -12,16 +14,27 @@ class Course extends Model
 
     protected $fillable = [
         "name",
-        "image"
+        "image",
+        "created_by"
     ];
-    
-    public function groups() : BelongsToMany
+
+    public function groups(): BelongsToMany
     {
         return $this->belongsToMany(Group::class, "course_group");
     }
 
-    public function topics() : HasMany 
+    public function users(): HasManyThrough
+    {
+        return $this->hasManyThrough(User::class, Group::class, "course_id", "user_id", "id", "group_id");
+    }
+
+    public function topics(): HasMany
     {
         return $this->hasMany(Topic::class);
+    }
+
+    public function creator(): BelongsTo
+    {
+        return $this->belongsTo(User::class, "created_by");
     }
 }
