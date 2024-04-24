8bd059ab7728fcdde9742cfa24184dad6aa72557
Laczkó Márk <diak_479.laczko.mark@2per14.b.kimeno.com>
2024-04-24 14:25:08 +0200

    Removed useless fields in the notes migration file

--- backend/database/migrations/2024_03_29_090849_create_notes_table.php
+++ backend/database/migrations/2024_03_29_090849_create_notes_table.php
@@ -15,8 +15,6 @@ return new class extends Migration
             $table->id();
             $table->string("title");
             $table->longText("text");
-            $table->unsignedBigInteger("courseable_id");
-            $table->string("courseable_type");
         });
     }
 
