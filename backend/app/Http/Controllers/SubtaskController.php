62564e816a7cbf791eec14995c6e54e2ad6daa62
Kurityák Dániel István <diak_478.kurityak.daniel.istvan@2per14.b.kimeno.com>
2024-04-25 09:32:29 +0000

    Updated SubtaskController

--- backend/app/Http/Controllers/SubtaskController.php
+++ backend/app/Http/Controllers/SubtaskController.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\SubtaskSolutionResource;
 use App\Models\Subtask;
 use Illuminate\Http\Request;
 
 class SubtaskController extends Controller
 {
+    public function solution(int $id){
+        $subtask = Subtask::findOrFail($id);
+
+        return new SubtaskSolutionResource($subtask);
+    }
+
     public function destroy(int $id){
         $subtask = Subtask::findOrFail($id);
         
