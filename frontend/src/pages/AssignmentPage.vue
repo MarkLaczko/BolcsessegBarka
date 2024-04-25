da955de0afa9a6e039e0a90efb83f62827235bd3
Molnár Gábor <diak_482.molnar.gabor@2per14.b.kimeno.com>
2024-04-25 14:42:45 +0200

    Adding basetoast

--- frontend/src/pages/AssignmentPage.vue
+++ frontend/src/pages/AssignmentPage.vue
@@ -2,6 +2,7 @@
   <BaseLayout>
 
     <BaseSpinner :loading="loading"/>
+    <BaseToast/>
 
     <div class="rounded-3 my-5 py-2" v-if="!loading">
       <div class="rounded-3 m-3 p-2 bg-white" v-for="assignment in assignments" :key="assignment.id">
@@ -14,175 +15,10 @@
         <div v-if="isDeadlineReached" class=" py-4 text-center alert alert-danger">
           {{ messages.pages.assignmentPage.deadlineExpired }}
         </div>
-        <div class="card" v-if="!isDeadlineReached">
-          <Toast
-            :pt="{
-              root: {
-                class: 'w-25',
-              },
-              detail: {
-                class: 'text-center',
-              },
-              icon: {
-                class: 'mt-1 ms-1',
-              },
-              text: {
-                class: 'w-75 mx-auto',
-              },
-              container: {
-                class: ' rounded w-75',
-              },
-              buttonContainer: {
-                class: 'w-25 d-flex justify-content-center ms-auto',
-              },
-              button: {
-                class: 'btn mb-2',
-              },
-              transition: {
-                name: 'slide-fade',
-              },
-            }"
-          />
-          <!-- <FileUpload
-            :pt="{
-              input: {
-                class: 'd-none',
-              },
-            }"
-            url="api/assignments"
-            @upload="onTemplatedUpload($event)"
-            :multiple="true"
-            accept=".zip, .rar, .jpg, .jpeg, .png"
-            :maxFileSize="100000000"
-            @select="onSelectedFiles"
-          >
-            <template
-              #header="{ chooseCallback, uploadCallback, clearCallback, files }"
-            >
-              <div
-                class="d-flex justify-content-between align-items-center flex-wrap flex-grow-1 mt-2"
-              >
-                <div class="d-flex gap-2 ms-2">
-                  <button
-                    @click="chooseCallback()"
-                    class="btn btn-outline-primary rounded-circle"
-                  >
-                    <i class="pi pi-images"></i>
-                  </button>
-                  <button
-                    @click="uploadEvent(uploadCallback)"
-                    class="btn btn-outline-success rounded-circle"
-                    :disabled="!files || files.length === 0 || totalSize >= 100"
-                  >
-                    <i class="pi pi-cloud-upload"></i>
-                  </button>
-                  <button
-                    @click="clearCallback()"
-                    class="btn btn-outline-danger rounded-circle"
-                    :disabled="!files || files.length === 0"
-                  >
-                    <i class="pi pi-times"></i>
-                  </button>
-                </div>
-                <div class="progress mb-3 w-25 me-3">
-                  <div
-                    class="progress-bar bg-success"
-                    role="progressbar"
-                    :style="{ width: totalSizePercent + '%' }"
-                    :aria-valuenow="totalSizePercent"
-                    aria-valuemin="0"
-                    aria-valuemax="100"
-                  >
-                    <span class="text-white">{{ totalSize }}MB / 100MB</span>
-                  </div>
-                </div>
-              </div>
-            </template>
-            <template
-              #content="{
-                files,
-                uploadedFiles,
-                removeUploadedFileCallback,
-                removeFileCallback,
-              }"
-            >
-              <div v-if="files.length > 0">
-                <h5 class="mt-3 ms-2">Függőben</h5>
-                <div
-                  class="row row-cols-1 row-cols-sm-2 row-cols-md-3 p-0 sm:p-5 gap-3 mx-2 mb-2"
-                >
-                  <div
-                    v-for="(file, index) of files"
-                    :key="file.name + file.type + file.size"
-                    class="col card m-0 px-4 py-3 border-1 surface-border align-items-center gap-3"
-                  >
-                    <div>
-                      <img
-                        role="presentation"
-                        :alt="file.name"
-                        :src="file.objectURL"
-                        class="img-fluid"
-                      />
-                    </div>
-                    <span class="font-weight-bold">{{ file.name }}</span>
-                    <div>{{ formatSize(file.size) }}</div>
-                    <span class="badge bg-warning">Függőben</span>
-                    <button
-                      @click="
-                        onRemoveTemplatingFile(file, removeFileCallback, index)
-                      "
-                      class="btn btn-outline-danger rounded-circle"
-                    >
-                      <i class="pi pi-times"></i>
-                    </button>
-                  </div>
-                </div>
-              </div>
-
-              <div v-if="uploadedFiles.length > 0">
-                <h5 class="mt-3 ms-2">Feltöltve</h5>
-                <div
-                  class="row row-cols-1 row-cols-sm-2 row-cols-md-3 p-0 sm:p-5 gap-3 mx-2 mb-2"
-                >
-                  <div
-                    v-for="(file, index) of uploadedFiles"
-                    :key="file.name + file.type + file.size"
-                    class="col card m-0 px-4 py-3 border-1 surface-border align-items-center gap-3"
-                  >
-                    <div>
-                      <img
-                        role="presentation"
-                        :alt="file.name"
-                        :src="file.objectURL"
-                        class="img-fluid"
-                      />
-                    </div>
-                    <span class="font-weight-bold">{{ file.name }}</span>
-                    <div>{{ formatSize(file.size) }}</div>
-                    <span class="badge bg-success">Feltöltve</span>
-                    <button
-                      @click="removeUploadedFileCallback(index)"
-                      class="btn btn-outline-danger rounded-circle"
-                    >
-                      <i class="pi pi-times"></i>
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </template>
-            <template #empty>
-              <div
-                class="d-flex align-items-center justify-content-center flex-column mb-2"
-              >
-                <i
-                  class="pi pi-cloud-upload border rounded-circle p-3 text-custom"
-                />
-                <p class="mt-4 mb-0">
-                  {{ messages.pages.assignmentPage.dragFileText }}
-                </p>
-              </div>
-            </template>
-          </FileUpload> -->
+        <div class="d-flex justify-content-end">
+          <RouterLink v-if="isDeadlineReached" class="btn btn-outline-danger px-5" :to="{ name: 'course', params: { id: assignment.course.id } ,}">{{messages.pages.assignmentPage.returnButton}}</RouterLink>
+        </div>
+        <div v-if="!isDeadlineReached">
           <FormKit
           type="form"
           :actions="false"
@@ -198,7 +34,7 @@
             :label="
               messages.pages.newAssignmentPage.teacher_task
             "
-            multiple="true"
+            multiple="false"
             validation=""
             :classes="{
               input: {
@@ -210,13 +46,26 @@
               },
             }"
           />
+          <div class="d-flex justify-content-end mt-3 mb-3">
+            <RouterLink class="btn btn-outline-danger px-5" :to="{ name: 'course', params: { id: assignment.course.id } ,}">{{messages.pages.assignmentPage.returnButton}}</RouterLink>
+            <FormKit
+              type="submit"
+              :label="
+                messages.pages.newAssignmentPage.saveButton
+              "
+              id="addAssignmentButton"
+              :classes="{
+                input: {
+                  btn: true,
+                  'btn-success': true,
+                  'w-auto': true,
+                  'px-5' : true,
+                  'ms-1' : true
+                },
+              }"
+            />
+          </div>
         </FormKit>
-        </div>
-        <div class="d-flex justify-content-end">
-          <RouterLink class="btn btn-outline-danger mt-3 px-5" :to="{ name: 'course', params: { id: assignment.course.id } ,}">{{messages.pages.assignmentPage.returnButton}}</RouterLink>
-          <button type="submit" class="btn btn-success mt-3 px-5 ms-1"  @click="postNewStudentAssignment">
-            {{ messages.pages.newAssignmentPage.saveButton }}
-          </button>
         </div>
       </div>
     </div>
@@ -235,6 +84,7 @@ import { languageStore } from "@stores/LanguageStore.mjs";
 import { http } from "@utils/http";
 import { userStore } from "@stores/UserStore";
 import BaseSpinner from "@components/BaseSpinner.vue"
+import BaseToast from "@components/BaseToast.vue";
 
 export default {
   components: {
@@ -244,7 +94,8 @@ export default {
     Button,
     Toast,
     ProgressBar,
-    BaseSpinner
+    BaseSpinner,
+    BaseToast
   },
   data() {
     return {
@@ -277,7 +128,7 @@ export default {
           formData.append('assignment_id', this.$route.params.id)
           formData.append('student_task_name', data.student_task[0].name);
           formData.append('student_task', data.student_task[0].file);
-
+          
         const user = userStore();
         await http.post(`/studentAssignments`, formData, {
           headers: {
@@ -310,54 +161,8 @@ export default {
           toast.styleClass = "bg-danger text-white";
         }
         this.$toast.add(toast);
-        console.log(data);
       }
     },
-    // onRemoveTemplatingFile(file, removeFileCallback, index) {
-    //   removeFileCallback(index);
-    //   this.totalSize -= parseFloat(this.formatSize(file.size));
-    //   this.totalSizePercent = this.totalSize;
-    // },
-    // onClearTemplatingUpload(clear) {
-    //   clear();
-    //   this.totalSize = 0;
-    //   this.totalSizePercent = 0;
-    // },
-    // onSelectedFiles(event) {
-    //   this.files = event.files;
-    //   this.files.forEach((file) => {
-    //     this.totalSize += parseFloat(this.formatSize(file.size));
-    //   });
-    // },
-    // uploadEvent(callback) {
-    //   this.totalSizePercent = this.totalSize;
-    //   callback();
-    // },
-    // onTemplatedUpload() {
-    //   this.$toast.add({
-    //     severity: "info",
-    //     summary: "Success",
-    //     detail: "File Uploaded",
-    //     life: 3000,
-    //   });
-    // },
-    // formatSize(bytes) {
-    //   const k = 1024;
-    //   const sizes = this.$primevue.config.locale.fileSizeTypes;
-
-    //   if (bytes === 0) {
-    //     return `0 ${sizes[0]}`;
-    //   }
-    //   else if (bytes <= 1000000) {
-    //     return `${(bytes/1000000).toFixed(3)} ${sizes[2]}`;
-    //   }
-    //   else {
-    //     const i = Math.floor(Math.log(bytes) / Math.log(k));
-    //     const formattedSize = parseFloat((bytes / Math.pow(k, i)).toFixed(0));
-
-    //     return `${formattedSize} ${sizes[i]}`;
-    //   }
-    // },
     checkDeadline() {
       const currentDateTime = new Date();
       const deadlineDateTime = new Date(Date.parse(this.deadlineDate));
