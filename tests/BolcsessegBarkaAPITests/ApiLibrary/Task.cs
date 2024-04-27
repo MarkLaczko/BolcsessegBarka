761f73a9fb4f6cf7f100e3247f82a3249ebfbb24
Kurityák Dániel István <diak_478.kurityak.daniel.istvan@2per14.b.kimeno.com>
2024-04-27 23:58:27 +0200

    Added Task class in API test

---
+++ tests/BolcsessegBarkaAPITests/ApiLibrary/Task.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace ApiLibrary;
+
+public class Task
+{
+    [JsonProperty("id")]
+    public int Id { get; set; }
+    
+    [JsonProperty("order")]
+    public int Order { get; set; }
+    
+    [JsonProperty("header")]
+    public string Header { get; set; }
+    
+    [JsonProperty("text")]
+    public string Text { get; set; }
+    
+    [JsonProperty("subtasks")]
+    public List<Subtask> Subtasks { get; set; }
+}
