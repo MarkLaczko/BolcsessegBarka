08abe122e1c83000254193e6533e89cdd9e870d8
Kurityák Dániel István <diak_478.kurityak.daniel.istvan@2per14.b.kimeno.com>
2024-04-27 14:12:57 +0000

    Added QuizStore

---
+++ frontend/src/stores/QuizStore.mjs
@@ -0,0 +1,49 @@
+import { defineStore } from "pinia";
+import { http } from "@utils/http";
+import { userStore } from "@stores/UserStore";
+
+export const quizStore = defineStore("quizStore", {
+  state() {
+    return {
+      
+    };
+  },
+  actions: {
+    async getQuiz(id) {
+        const user = userStore();
+        const response = await http.get(`quizzes/${id}`, {
+            headers: {
+                Authorization: `Bearer ${user.token}`,
+            },
+        });
+        return response.data.data;
+    },
+    async postQuiz(data) {
+      const user = userStore();
+      const response = await http.post(`quizzes`, data, {
+        headers: {
+            Authorization: `Bearer ${user.token}`,
+        },
+    });
+      return response.data.data;
+    },
+    async putQuiz(id, data) {
+      const user = userStore();
+      const response = await http.put(`/quizzes/${id}`, data, {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      });
+
+      return response.data.data;
+    },
+    async destroyQuiz(id) {
+      const user = userStore();
+      const response = await http.delete(`/quizzes/${id}`, {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      });
+    },
+  },
+});
