f663a9e4b334e9da1bc84de5c5c9b5e70f904f1f
Kurityák Dániel István <diak_478.kurityak.daniel.istvan@2per14.b.kimeno.com>
2024-04-27 23:58:15 +0200

    Added Subtask class in API test

---
+++ tests/BolcsessegBarkaAPITests/ApiLibrary/Subtask.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace ApiLibrary;
+
+public class Subtask
+{
+    [JsonProperty("id")]
+    public int Id { get; set; }
+    
+    [JsonProperty("order")]
+    public int Order { get; set; }
+    
+    [JsonProperty("question")]
+    public string Question { get; set; }
+    
+    [JsonProperty("options")]
+    public List<string>? Options { get; set; }
+    
+    [JsonProperty("type")]
+    public string Type { get; set; }
+    
+    [JsonProperty("marks")]
+    public double Marks { get; set; }
+}
