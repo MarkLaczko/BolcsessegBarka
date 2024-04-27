faa9818f1c51d66e7ee94179ded9ab5769cf063e
Kurityák Dániel István <diak_478.kurityak.daniel.istvan@2per14.b.kimeno.com>
2024-04-27 23:25:51 +0200

    Added QuizTopicCourse class in API test

---
+++ tests/BolcsessegBarkaAPITests/ApiLibrary/QuizTopicCourse.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+
+namespace ApiLibrary;
+
+public class QuizTopicCourse
+{
+    [JsonProperty("id")]
+    public int Id { get; set; }
+    
+    [JsonProperty("name")]
+    public string Name { get; set; }
+    
+    [JsonProperty("max_attempts")]
+    public int MaxAttempts { get; set; }
+    
+    [JsonProperty("opens")]
+    public int Opens { get; set; }
+    
+    [JsonProperty("closes")]
+    public int Closes { get; set; }
+    
+    [JsonProperty("time")]
+    public int Time { get; set; }
+    
+    [JsonProperty("number_of_tasks")]
+    public int NumberOfTasks { get; set; }
+    
+    [JsonProperty("topic")]
+    public TopicQuiz Topic { get; set; }
+}
