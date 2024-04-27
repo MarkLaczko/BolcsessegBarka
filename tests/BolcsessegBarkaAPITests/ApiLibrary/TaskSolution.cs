0368a2dad508490a62a2e7f15af2e31d16137283
Kurityák Dániel István <diak_478.kurityak.daniel.istvan@2per14.b.kimeno.com>
2024-04-28 00:13:59 +0200

    Added TaskSolution class in API test

---
+++ tests/BolcsessegBarkaAPITests/ApiLibrary/TaskSolution.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace ApiLibrary;
+
+public class TaskSolution
+{
+    [JsonProperty("id")]
+    public int Id { get; set; }
+    
+    [JsonProperty("solution")]
+    public List<string>? Solution { get; set; }
+}
