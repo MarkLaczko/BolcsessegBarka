f8235a4029024f333815e8ec92117f273f37a035
Laczkó Márk <diak_479.laczko.mark@2per14.b.kimeno.com>
2024-04-27 09:19:02 +0200

    The fix for the Dark Mode functionality bug: the theme is lost upon page reload

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,26 @@
 <script>
 import { RouterView } from "vue-router";
 import ScrollBackToTopButton from "@components/ScrollBackToTopButton.vue";
+import { themeStore } from "@stores/ThemeStore.mjs";
+import { mapState } from "pinia";
 export default {
   components: {
     RouterView,
     ScrollBackToTopButton
   },
   computed: {
+    ...mapState(themeStore, ["isDarkMode"]),
     isSignedOut() {
       return this.$route.path === "/register" || this.$route.path === "/login";
     },
   },
+  mounted() {
+    if (this.isDarkMode) {
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    document.documentElement.removeAttribute("data-theme");
+  }
+  }
 };
 </script>
 
