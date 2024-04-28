ee614b5ff669e1d4837843a6c200ac9f534b25ac
Kurityák Dániel István <diak_478.kurityak.daniel.istvan@2per14.b.kimeno.com>
2024-04-28 20:56:55 +0000

    Added Answer model

---
+++ backend/app/Models/Answer.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Answer extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'attempt_id',
+        'subtask_id',
+        'answer',
+        'correct',
+    ];
+
+    public $timestamps = false;
+
+    public function attempt() : BelongsTo 
+    {
+        return $this->belongsTo(Attempt::class);
+    }
+
+    public function subtask() : BelongsTo
+    {
+        return $this->belongsTo(Subtask::class);
+    }
+}
