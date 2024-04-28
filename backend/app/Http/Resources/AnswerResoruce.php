0d279ce1c7f5802c15a4f23857fdd8847f5110d2
Kurityák Dániel István <diak_478.kurityak.daniel.istvan@2per14.b.kimeno.com>
2024-04-28 20:58:48 +0000

    Added AnswerResource

---
+++ backend/app/Http/Resources/AnswerResoruce.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AnswerResoruce extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'attempt' => new AttemptResoruce($this->whenLoaded('attempt')),
+            'subtask' => new SubtaskResource($this->whenLoaded('subtask')),
+            'answer' => $this->answer,
+            'correct' => $this->correct,
+        ];
+    }
+}
