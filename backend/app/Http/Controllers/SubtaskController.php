a4061c47512377788d75508f6ee157e00959573c
Kurityák Dániel István <diak_478.kurityak.daniel.istvan@2per14.b.kimeno.com>
2024-04-28 18:23:15 +0000

    Added gates to SubtaskController

--- backend/app/Http/Controllers/SubtaskController.php
+++ backend/app/Http/Controllers/SubtaskController.php
@@ -5,17 +5,20 @@ namespace App\Http\Controllers;
 use App\Http\Resources\SubtaskSolutionResource;
 use App\Models\Subtask;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class SubtaskController extends Controller
 {
     public function solution(int $id){
         $subtask = Subtask::findOrFail($id);
+        Gate::authorize("subtasks.solutions", $subtask);
 
         return new SubtaskSolutionResource($subtask);
     }
 
     public function destroy(int $id){
         $subtask = Subtask::findOrFail($id);
+        Gate::authorize("tasks.delete", $subtask);
         
         $subtask->delete();
 
