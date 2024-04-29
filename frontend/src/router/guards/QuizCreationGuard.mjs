14b1380f5784907b29e158573af5a4b08dc6c8d3
Kurityák Dániel István <diak_478.kurityak.daniel.istvan@2per14.b.kimeno.com>
2024-04-29 11:54:15 +0000

    Fixed QuizCreationGuard

--- frontend/src/router/guards/QuizCreationGuard.mjs
+++ frontend/src/router/guards/QuizCreationGuard.mjs
@@ -1,48 +1,39 @@
 import { userStore } from "@stores/UserStore.mjs";
 import { courseStore } from "@stores/CourseStore.mjs";
-import { http } from '@utils/http';
+import { quizStore } from "@stores/QuizStore.mjs";
 
 async function getCourse() {
     const path = location.pathname.split('/');
-    if(path[5] != "create-quiz" && (path[1] != "quiz" && path[3] != "edit")){
+    if((path[1] != "quiz" && path[3] != "edit") && (path[5] != 'create-quiz')){
         return false;
     }
+    
+    let permissions = [];
+
+    if(path[1] == 'quiz'){
+        let quizId = path[2];
+        const response = await quizStore().getQuiz(quizId);
+        for (const group of response.topic.course.groups) {
+            const user = group.users.find(x => x.id == userStore().currentUserData.id);
+            if(user != undefined){
+                permissions.push(user.member.permission);
+            }
+        }
+        return permissions.includes("Tanár");
+    }
+    
     let courseId;
     if(path[5] == "create-quiz") {
         courseId = path[2];
-    }
-    else {
-        await courseStore().getCourses();
-        for (const course of courseStore().courses) {
-            for (const topic of course.topics) {
-                const quiz = topic.quizzes.find(x => x.id == path[2]);
-                if(quiz != undefined) {
-                    courseId = course.id;
-                }
+        const response2 = await courseStore().getCourse(courseId);
+        for (const group of response2.groups) {
+            const user = group.users.find(x => x.id == userStore().currentUserData.id);
+            if(user != undefined){
+                permissions.push(user.member.permission);
             }
         }
-        if(courseId == null) {
-            return false;
-        }
+        return permissions.includes("Tanár");
     }
-    let groups = [];
-    const response = await http.get('/groups', {
-        headers: {
-            Authorization: `Bearer ${userStore().token}`,
-        },
-    });
-    groups = response.data.data;
-    let groupContainsCourse = groups.filter(x => x.courses.find(y => y.id == courseId));
-
-    let permissions = [];
-    for(const group of groupContainsCourse) {
-        let userGroups = group.users.find(x => x.id == userStore().currentUserData.id);
-        if(userGroups != undefined){
-            permissions.push(userGroups.member.permission);
-        }
-    }
-
-    return permissions.includes("Tanár");
 }
 
 export async function QuizCreationGuard(to, from, next) {
