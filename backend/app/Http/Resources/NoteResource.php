473ece5d12d0cc7cb4b03ffe72ce02b185dccdee
Laczkó Márk <diak_479.laczko.mark@2per14.b.kimeno.com>
2024-04-29 10:26:50 +0200

    Returning the current topic_id with the current note

--- backend/app/Http/Resources/NoteResource.php
+++ backend/app/Http/Resources/NoteResource.php
@@ -18,7 +18,8 @@ class NoteResource extends JsonResource
             "id" => $this->id,
             "title" => $this->title,
             "text" => $this->text,
-            "user_id" => $this->user_id
+            "user_id" => $this->user_id,
+            "topic_id" => $this->topic_id
         ];
     }
 }
