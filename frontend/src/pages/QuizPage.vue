495ac826c07ca08d54cfe030ce4843f1b96aa1cc
Kurityák Dániel István <diak_478.kurityak.daniel.istvan@2per14.b.kimeno.com>
2024-04-29 21:30:18 +0000

    Updated QuizPage

--- frontend/src/pages/QuizPage.vue
+++ frontend/src/pages/QuizPage.vue
@@ -2,49 +2,80 @@
     <BaseLayout>
         <BaseSpinner v-if="loading" />
         <div v-if="!loading">
-            <h1>{{ quiz.value.name }}</h1>
-            <table class="mt-2 mb-0 table table-striped text-center">
-                <tbody>
-                    <tr>
-                        <td class="w-50">
-                            {{ messages.pages.quizPage.opens }}
-                        </td>
-                        <td class="w-50" v-if="quiz.value.opens != null">
-                            {{ toDate(quiz.value.opens) }}
-                        </td>
-                        <td class="w-50" v-else>-</td>
-                    </tr>
-                    <tr>
-                        <td class="w-50">
-                            {{ messages.pages.quizPage.closes }}
-                        </td>
-                        <td class="w-50" v-if="quiz.value.closes != null">
-                            {{ toDate(quiz.value.closes) }}
-                        </td>
-                        <td class="w-50" v-else>-</td>
-                    </tr>
-                    <tr>
-                        <td class="w-50">
-                            {{ messages.pages.quizPage.time }}
-                        </td>
-                        <td class="w-50" v-if="quiz.value.time != null">
-                            {{ quiz.value.time }}
-                            {{ messages.pages.quizPage.quiz.minutes }}
-                        </td>
-                        <td class="w-50" v-else>-</td>
-                    </tr>
-                    <tr>
-                        <td class="w-50">
-                            {{ messages.pages.quizPage.attempts }}
-                        </td>
-                        <td class="w-50" v-if="quiz.value.max_attempts != null">
-                            {{ quiz.value.max_attempts }}
-                        </td>
-                        <td class="w-50" v-else>-</td>
-                    </tr>
-                </tbody>
-            </table>
-            <div class="row" v-if="!isTeacher && attempts.value != []">
+
+            <BaseDialog
+                :visible="beginAttemptDialogVisible"
+                :header="messages.pages.quizPage.beginAttemptDialog.begin"
+            >
+                <div>
+                    <p class="mb-1">{{ messages.pages.quizPage.beginAttemptDialog.confirm }}</p>
+                    <p class="mb-1" v-if="quiz.value.time != null">{{ messages.pages.quizPage.beginAttemptDialog.time }}: {{ quiz.value.time }} {{ messages.pages.quizPage.beginAttemptDialog.minutes }}.</p>
+                    <p class="mb-1" v-if="quiz.value.max_attempts == 1">{{ messages.pages.quizPage.beginAttemptDialog.attemptOnce }}</p>
+                    <p class="mb-1" v-else>{{ messages.pages.quizPage.beginAttemptDialog.attemptMultipleStart }} {{ quiz.value.max_attempts == null ? messages.pages.quizPage.beginAttemptDialog.attemptMultipleMiddle : quiz.value.max_attempts }} {{ messages.pages.quizPage.beginAttemptDialog.attemptMultipleEnd }}.</p>
+                </div>
+                <div class="d-flex justify-content-end align-items-center gap-2">
+                    <button type="button" class="btn btn-outline-danger" @click="beginAttemptDialogVisible = false">{{ messages.pages.quizPage.beginAttemptDialog.cancel }}</button>
+                    <button type="button" class="btn btn-success" @click="startAttempt">{{ messages.pages.quizPage.beginAttemptDialog.start }}</button>
+                </div>
+            </BaseDialog>
+
+            <div class="row">
+                <div class="col-12">
+                    <h1>{{ quiz.value.name }}</h1>
+                </div>
+                <div class="col-12">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><RouterLink :to="{name: 'course', params: {id: quiz.value.topic.course.id}}">{{ quiz.value.topic.course.name }}</RouterLink></li>
+                        <li class="breadcrumb-item"><RouterLink :to="{name: 'course', params: {id: quiz.value.topic.course.id}}">{{ quiz.value.topic.name }}</RouterLink></li>
+                        <li class="breadcrumb-item active" aria-current="page">{{ quiz.value.name }}</li>
+                    </ol>
+                </div>
+                <div class="col-12">
+                    <table class="mt-2 mb-0 table table-striped text-center">
+                        <tbody>
+                            <tr>
+                                <td class="w-50">
+                                    {{ messages.pages.quizPage.opens }}
+                                </td>
+                                <td class="w-50" v-if="quiz.value.opens != null">
+                                    {{ toDate(quiz.value.opens) }}
+                                </td>
+                                <td class="w-50" v-else>-</td>
+                            </tr>
+                            <tr>
+                                <td class="w-50">
+                                    {{ messages.pages.quizPage.closes }}
+                                </td>
+                                <td class="w-50" v-if="quiz.value.closes != null">
+                                    {{ toDate(quiz.value.closes) }}
+                                </td>
+                                <td class="w-50" v-else>-</td>
+                            </tr>
+                            <tr>
+                                <td class="w-50">
+                                    {{ messages.pages.quizPage.time }}
+                                </td>
+                                <td class="w-50" v-if="quiz.value.time != null">
+                                    {{ quiz.value.time }}
+                                    {{ messages.pages.quizPage.minutes }}
+                                </td>
+                                <td class="w-50" v-else>-</td>
+                            </tr>
+                            <tr>
+                                <td class="w-50">
+                                    {{ messages.pages.quizPage.attempts }}
+                                </td>
+                                <td class="w-50" v-if="quiz.value.max_attempts != null">
+                                    {{ quiz.value.max_attempts }}
+                                </td>
+                                <td class="w-50" v-else>-</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            
+            <div class="row" v-if="isTeacher && attempts.value != []">
                 <div class="col-12 mt-2">
                     <h2>{{ messages.pages.quizPage.userAttempts }}</h2>
                     <table class="table table-responsive table-striped align-middle">
@@ -67,7 +98,7 @@
                                 <td>{{ attempt.marks }}</td>
                                 <td>{{ attempt.grade }}</td>
                                 <td>
-                                    <button type="button" class="btn btn-primary">Értékelés</button>
+                                    <button type="button" class="btn btn-primary">{{ messages.pages.quizPage.evaluate }}</button>
                                 </td>
                             </tr>
                         </tbody>
@@ -75,8 +106,33 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-12" v-if="quiz.value.closes != null || (quiz.value.closes < Math.floor(Date.now() / 1000))">
-                    <button type="button" class="btn btn-primary">Asd</button>
+                <div class="col-12 mt-2 d-flex justify-content-center aling-items-center" v-if="!hasAttemptOnThisQuiz && (quiz.value.opens == null || quiz.value.opens <= Math.floor(Date.now() / 1000)) && (quiz.value.closes == null || quiz.value.closes >= Math.floor(Date.now() / 1000))">
+                    <button type="button" class="btn btn-primary" @click="beginAttemptDialogVisible = true">{{ messages.pages.quizPage.startAttempt }}</button>
+                </div>
+                <div class="col-12 mt-2" v-if="userAttempts.length > 0">
+                    <h2>{{ messages.pages.quizPage.ownAttempts }}</h2>
+                    <table class="table table-striped table-responsive">
+                        <thead>
+                            <tr>
+                                <th>Kezdés</th>
+                                <th>Végzés</th>
+                                <th>Pontszám</th>
+                                <th>Érdemjegy</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="attempt of userAttempts" :key="attempt.id">
+                                <td>{{ toDate(attempt.start) }}</td>
+                                <td v-if="attempt.end != null">{{ toDate(attempt.end) }}</td>
+                                <td v-else>-</td>
+                                <td>{{ attempt.marks }}</td>
+                                <td>{{ attempt.grade }}</td>
+                                <td v-if="attempt.end == null"><a href="#">Folytatás</a></td>
+                                <td v-else></td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
@@ -86,20 +142,29 @@
 <script setup>
 import BaseLayout from '@layouts/BaseLayout.vue';
 import BaseSpinner from '@components/BaseSpinner.vue';
+import BaseDialog from '@components/BaseDialog.vue';
 import { quizStore } from '@stores/QuizStore';
 import { languageStore } from '@stores/LanguageStore';
 import { userStore } from '@stores/UserStore';
 import { attemptStore } from '@stores/AttemptStore';
-import { reactive, computed, onMounted } from 'vue';
+import { ref, reactive, computed, onMounted } from 'vue';
 import { useRoute } from 'vue-router';
 
 const route = useRoute();
 
-const messages = languageStore().messages;
+const messages = reactive(languageStore().messages);
 
 const quiz = reactive([]);
 
 const attempts = reactive([]);
+
+const beginAttemptDialogVisible = ref(false);
+
+const hasAttemptOnThisQuiz = computed(() => {
+    return attemptStore().userAttempts.findIndex(x => x.quiz.id == route.params.id && x.user.id == userStore().currentUserData.id && x.end == null) != -1;
+});
+
+const userAttempts = reactive(attemptStore().attempts);
 
 const loading = computed(() => {
     return quiz.value == [] || quiz.value == undefined;
@@ -125,10 +190,14 @@ const getAttempts = async () => {
     }
 }
 
+const startAttempt = async () => {
+    await attemptStore().postAttempt(route.params.id);
+}
+
 onMounted(async () => {
     quiz.value = await quizStore().getQuiz(route.params.id)
-    await getAttempts()
-    console.log(Math.floor(Date.now() / 1000))
+    await getAttempts();
+    await attemptStore().getUsersAttempts();
 })
 
 </script>
