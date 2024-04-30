3d3425d278478c391ab23ea33d152c2e4dcf9699
Laczkó Márk <diak_479.laczko.mark@2per14.b.kimeno.com>
2024-04-30 22:14:41 +0200

    Writing the assignment seeder

---
+++ backend/database/seeders/AssignmentSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+use function Laravel\Prompts\table;
+
+class AssignmentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('assignment')->insert([
+            [
+                'task_name' => 'Ókori civilizációk kutatása',
+                'comment' => 'Az ókori Egyiptom, Görögország és Róma társadalmi szerkezete és kultúrája.',
+                'deadline' => now()->addWeeks(2),
+                'courseable_id' => 1,
+                'topic_id' => 1,
+                'teacher_task_name' => 'Kutatási projekt'
+            ],
+            [
+                'task_name' => 'Középkori Európa elemzése',
+                'comment' => 'Elemzés a középkori Európa politikai és gazdasági viszonyairól.',
+                'deadline' => now()->addWeeks(3),
+                'courseable_id' => 1,
+                'topic_id' => 1,
+                'teacher_task_name' => 'Esszé írása'
+            ],
+            [
+                'task_name' => 'Modern korszak fordulópontjai',
+                'comment' => 'A jelentős történelmi fordulópontok hatása a modern társadalomra.',
+                'deadline' => now()->addWeeks(4),
+                'courseable_id' => 1,
+                'topic_id' => 1,
+                'teacher_task_name' => 'Csoportos prezentáció'
+            ]
+        ]);
+    }
+}
