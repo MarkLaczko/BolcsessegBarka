6f16e3a24922b5b63488350c48f53e9baa4960c6
Kurityák Dániel István <diak_478.kurityak.daniel.istvan@2per14.b.kimeno.com>
2024-04-30 09:53:15 +0000

    Updated api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -197,6 +197,9 @@ Route::middleware('auth:sanctum')->delete('/attempts/{id}', [AttemptController::
     ->name('attempts.destroy');
 Route::middleware('auth:sanctum')->get('/user/attempts', [AttemptController::class,'userAttempts'])
     ->name('attempts.userAttempts');
+Route::middleware('auth:sanctum')->get('/attempts/{id}/quiz', [AttemptController::class,'getAttemptQuizDetails'])
+    ->whereNumber('id')
+    ->name('attempts.getAttemptQuizDetails');
 
 Route::middleware('auth:sanctum')->get('/answers', [AnswerController::class,'index'])
     ->name('answers.index');
