24353b859bcfdf6aea2db30baf29e7ea8d09f2eb
Molnár Gábor <diak_482.molnar.gabor@2per14.b.kimeno.com>
2024-05-04 12:12:32 +0200

    Adding teacher_task_name if the teacher not upload the new file when her update it

--- frontend/src/pages/CoursePage.vue
+++ frontend/src/pages/CoursePage.vue
@@ -1694,7 +1694,8 @@ export default {
           new Date(data.deadline).toISOString().slice(0, 19).replace("T", " ")
         );
         formData.append("teacher_task", data?.teacher_task[0]?.file);
-        formData.append("teacher_task_name", data?.teacher_task[0]?.name);
+        formData.append("teacher_task_name", data?.teacher_task[0]?.name == null ? this.currentAssignment.teacher_task_name : data?.teacher_task[0]?.name);
+       
 
         const user = userStore();
         await http.put(`/assignments/${this.currentAssignment.id}`, formData, {
